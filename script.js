9a4a2e67eb88203e17042d6d7d840a0340e6900d
Uttam Maji <uttammaji842@gmail.com>
2024-12-24 23:48:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Function to change the message when the button is clicked
+function changeMessage() {
+    const messageElement = document.getElementById('message');
+    const personalMessageElement = document.getElementById('personalMessage');
+    
+    messageElement.textContent = "You're the best In Everythings  🎁";
+    personalMessageElement.textContent = "May i wish you your all problem is end in end this year and i want you alwayes keep happy || Happy Merry Christmas  ❤️";
+    
+    document.getElementById('surpriseButton').style.display = 'none';  // Hide the button after clicking
+}
